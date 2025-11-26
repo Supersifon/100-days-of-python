38ada4b4d076f2b5b5940c3850a2030a344df5a5
Supersifon <diegoocete@yahoo.com.ar>
2025-11-26 14:23:40 -0300

    Day 1: Band name generator project started

---
+++ day-01/BandNameGenerator.001.py
@@ -0,0 +1,6 @@
+# Band Name generator Project
+print("Welcome to the band name generator")
+city = input("Which is the city you grew up ? \n")
+pet = input("What is the name of your first pet? \n")
+print("You band name could be: " + city + " " + pet)
+
